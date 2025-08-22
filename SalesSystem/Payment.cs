aa5ceaf25e77aa3b81f4a77b51b32ba051bf6a51
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-08-22 10:22:58 +0200

    Add payment logic

--- SalesSystem/Payment.cs
+++ SalesSystem/Payment.cs
@@ -5,6 +5,22 @@ namespace SalesSystem
 {
     internal class Payment
     {
+        public static void SaveSummaryToSalesLog(string summaryFilePath, string salesLogFilePath)
+        {
+            if (File.Exists(summaryFilePath))
+            {
+                using (StreamReader reader = new StreamReader(summaryFilePath))
+                using (StreamWriter writer = new StreamWriter(salesLogFilePath, append: true))
+                {
+                    string? line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+
         public static void ClearSummary(string summaryFilePath)
         {
             // Overwrite the summary file with nothing
@@ -12,3 +28,4 @@ namespace SalesSystem
         }
     }
 }
+    
